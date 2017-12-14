bb45d5ccd162374e74a800c5f36fbe47dbc03135
Brenton Horne <brentonhorne77@gmail.com>
2017-12-14 20:10:28 +1000

    Creating branch with current config.scm and sudoers files; had to chown fusion809 everything in order to be able to commit with ease

--- config.scm
+++ config.scm
@@ -2,38 +2,49 @@
 ;; for a "desktop" setup with GNOME and Xfce where the
 ;; root partition is encrypted with LUKS.
 
-(use-modules (gnu) (gnu system nss))
+(use-modules 
+  (gnu)
+  (gnu system nss)
+  (gnu packages base)
+  (gnu packages shells)
+  )
 (use-service-modules desktop)
-(use-service-modules xorg)
 (use-package-modules certs gnome)
+(use-package-modules shells)
 
 (operating-system
   (host-name "fusion809-vbox")
   (timezone "Australia/Brisbane")
   (locale "en_AU.utf8")
+;;  (shell "/run/current-system/profile/bin/zsh")
 
   ;; Assuming /dev/sdX is the target hard disk, and "my-root"
   ;; is the label of the target root file system.
-  (bootloader (grub-configuration (target "/dev/sda")))
+  (bootloader (bootloader-configuration
+                (bootloader grub-bootloader)
+                (target "/dev/sda")))
 
+  ;; Specify a mapped device for the encrypted root partition.
+  ;; The UUID is that returned by 'cryptsetup luksUUID'.
   (file-systems (cons (file-system
-                        (device "my-root")
-                        (title 'label)
+                        (device "guixsd")
                         (mount-point "/")
-                        (type "ext4"))
+                        (type "btrfs"))
                       %base-file-systems))
 
   (users (cons (user-account
                 (name "fusion809")
-                (comment "Admin")
+                (comment "Brenton Horne's user")
                 (group "users")
                 (supplementary-groups '("wheel" "netdev"
                                         "audio" "video"))
-                (home-directory "/home/fusion809"))
+                (home-directory "/home/fusion809")
+		(shell "/run/current-system/profile/bin/zsh"))
                %base-user-accounts))
 
   ;; This is where we specify system-wide packages.
   (packages (cons* nss-certs         ;for HTTPS access
+		   zsh               ; for Z shell
                    gvfs              ;for user mounts
                    %base-packages))
 
@@ -42,16 +53,8 @@
   ;; include the X11 log-in service, networking with Wicd,
   ;; and more.
   (services (cons* (gnome-desktop-service)
-                   (console-keymap-service "us")
-                   (modify-services %desktop-services
-		     (slim-service-type config =>
-                       (slim-configuration (inherit config)
-					   (default-user "fusion809")
-					   (auto-login? #t)))
-                     (guix-service-type config =>
-                       (guix-configuration
-                         (inherit config)
-                         (use-substitutes? #f)
-                         (extra-options '("--cores=4" "--max-jobs=8")))))))
+                   %desktop-services))
+
   (sudoers-file (local-file "/etc/guix/sudoers"))
+  ;; Allow resolution of '.local' host names with mDNS.
   (name-service-switch %mdns-host-lookup-nss))
